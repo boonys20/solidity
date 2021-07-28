2367680614b53c5c5a9afa8ca47bd90bef82e983
Kung Devz <kungcs20@gmail.com>
2021-07-28 21:39:13 +0700

    update test case

--- contracts/ExceptionExample.sol
+++ contracts/ExceptionExample.sol
@@ -1,19 +1,30 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
+
 contract ExceptionExample {
 
-    mapping ( address => uint) public balanceReceived;
+    mapping(address => uint256) public balanceReceived;
 
     function receiveMoney() public payable {
+        console.log("msg.value is %s", msg.value);
+        console.log("msg.sender is %s", msg.sender);
         balanceReceived[msg.sender] += msg.value;
+        console.log("Sender balance is %s tokens", balanceReceived[msg.sender]);
     }
 
-    function withDrawMoney(address payable _to, uint _amount) public {
+    function withDrawMoney(address payable _to, uint256 _amount) public {
+
+        console.log("Balance in %s is %s tokens", _to, balanceReceived[_to]);
+        console.log("Trying to send %s tokens to %s", _amount, _to);
+
         if (_amount <= balanceReceived[msg.sender]) {
             balanceReceived[msg.sender] -= _amount;
             _to.transfer(_amount);
         }
+
+        console.log("Balance in %s is %s tokens", _to, balanceReceived[_to]);
     }
 
 }
---
+++ test/ExceptionExample.js
@@ -0,0 +1,44 @@
+const { expect } = require("chai");
+
+describe("Token contract", function () {
+
+    let Contract;
+    let Token;
+    let owner;
+    let addr1;
+    let addr2;
+    let addrs;
+
+    beforeEach(async function () {
+        Contract = await ethers.getContractFactory("ExceptionExample");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        Token = await Contract.deploy();
+        await Token.deployed();
+    });
+
+
+    describe("Transactions", function () {
+
+        it(`should deposit 50 eth to weth contract:`, async () => {
+            await Token.receiveMoney({ value: ethers.utils.parseEther("50") });
+            const addrBalance = await Token.balanceReceived(
+                owner.address
+            );
+            expect(addrBalance).to.equal(ethers.utils.parseEther("50"));
+        });
+
+        it("should withdraw money 20 eth ", async function () {
+
+            await Token.receiveMoney({ value: ethers.utils.parseEther("20") });
+        
+            await Token.withDrawMoney(owner.address, ethers.utils.parseEther("20"));
+            const addrBalance = await Token.balanceReceived(
+                owner.address
+            );
+
+            expect(addrBalance).to.equal(ethers.utils.parseEther("0"));
+        });
+
+    });
+
+});
